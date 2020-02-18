f89140563ea4b095fbd1893f619a618c4c271b85
Bennie-Han <59855334+Bennie-Han@users.noreply.github.com>
2020-02-18 20:55:19 +0800

    Update cutmix.py

--- cutmix-mixup-ohemloss/cutmix.py
+++ cutmix-mixup-ohemloss/cutmix.py
@@ -1,4 +1,13 @@
+import numpy as np
+import torch
+
 def rand_bbox(size, lam):
+    """
+
+    :param size: torch.Size. Shape:(batch_size, channel, image_size, image_size)
+    :param lam: cutmix rate
+    :return: box boundary
+    """
     W = size[2]
     H = size[3]
     cut_rat = np.sqrt(1. - lam)
@@ -14,8 +23,17 @@ def rand_bbox(size, lam):
     bbx2 = np.clip(cx + cut_w // 2, 0, W)
     bby2 = np.clip(cy + cut_h // 2, 0, H)
 
-
+    return bbx1, bby1, bbx2, bby2
 def cutmix(data, targets1, targets2, targets3, alpha):
+    """
+
+    :param data: torch.Size. Shape:(batch_size, channel, image_size, image_size)
+    :param targets1: torch.Tensor. multitask, multi-classification, target1. shape(batch_size, 1)
+    :param targets2: torch.Tensor. multitask, multi-classification, target2. shape(batch_size, 1)
+    :param targets3: torch.Tensor. multitask, multi-classification, target3. shape(batch_size, 1)
+    :param alpha: float, positive
+    :return: resulting image, multi-target
+    """
     indices = torch.randperm(data.size(0))
     # shuffled_data = data[indices]
     shuffled_targets1 = targets1[indices]
